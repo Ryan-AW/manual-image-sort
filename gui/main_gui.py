a23feac1343c4d743be10c268f0946a89e9f4e61
Ryan-AW <ryanawhittock@outlook.com>
2024-06-21 06:12:14 +0100

    implemented read-only info frames

--- gui/main_gui.py
+++ gui/main_gui.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 from .frames.directories_frame import DirectoriesFrame
+from .frames.info_frame import InfoFrame
 
 class MainGui(tk.Frame):
     def __init__(self, master):
@@ -9,5 +10,8 @@ class MainGui(tk.Frame):
         self.create_widgets()
 
     def create_widgets(self):
+        self.info_frame = InfoFrame(self)
+        self.info_frame.pack()
+
         self.directory_selector = DirectoriesFrame(self)
         self.directory_selector.pack()
