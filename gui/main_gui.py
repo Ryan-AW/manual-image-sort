cd87d2099d53f5b0ec3ea1d07d1a236856ffedae
Ryan-AW <ryanawhittock@outlook.com>
2024-06-24 07:00:38 +0100

    linted main gui

--- gui/main_gui.py
+++ gui/main_gui.py
@@ -1,27 +1,27 @@
+''' compose a gui out of various independent frames '''
 import tkinter as tk
 from .frames.directories_frame import DirectoriesFrame
 from .frames.info_frame import InfoFrame
 from .frames.image_frame import ImageFrame
 
 class MainGui(tk.Frame):
+    ''' combine various frames into the gui '''
     def __init__(self, master):
         super().__init__(master)
         self.master = master
         self.grid(row=0, column=0, sticky='nsew')
-        self.create_widgets()
 
-    def create_widgets(self):
         self._left_frame = tk.Frame(self)
         self._left_frame.grid(row=0, column=0, sticky='nsew')
 
-        self.info_frame = InfoFrame(self._left_frame)
-        self.info_frame.grid(row=0, column=0, sticky='nsew')
+        self._info_frame = InfoFrame(self._left_frame)
+        self._info_frame.grid(row=0, column=0, sticky='nsew')
 
-        self.directory_selector = DirectoriesFrame(self._left_frame)
-        self.directory_selector.grid(row=1, column=0, sticky='nsew')
+        self._directory_selector = DirectoriesFrame(self._left_frame)
+        self._directory_selector.grid(row=1, column=0, sticky='nsew')
 
-        self.image_frame = ImageFrame(self)
-        self.image_frame.grid(row=0, column=1, sticky='nsew')
+        self._image_frame = ImageFrame(self)
+        self._image_frame.grid(row=0, column=1, sticky='nsew')
 
 
 
