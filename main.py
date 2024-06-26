06449ed7fa30345917e2ea1bb6bf919781a28f80
Ryan-AW <ryanawhittock@outlook.com>
2024-06-26 09:58:02 +0100

    added mainloop function

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 ''' a program for manually sorting images into directories '''
 from config.config_manager import ConfigManager
-from gui.main_gui import init_gui
+from gui.main_gui import init_gui, mainloop
 from utils.image_info import ImageInfo
 
 
 def main():
     ''' initialize program '''
     ConfigManager()
-    ImageInfo().open('resources/img_not_found.png')
+    ImageInfo()
+
     init_gui()
+
+    mainloop()
 
 
 if __name__ == "__main__":
