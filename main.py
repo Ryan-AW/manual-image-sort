389de1c56138fbfad1fc3918695d9ff2b7eab791
Ryan-AW <ryanawhittock@outlook.com>
2024-06-29 06:09:58 +0100

    initialize singleton image array class

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 ''' a program for manually sorting images into directories '''
 from config import ConfigManager
 from gui import init_gui, mainloop
-from utils import ImageInfo
+from utils import ImageInfo, ImageArray
 
 
 def main():
     ''' initialize program '''
     ConfigManager()
+    ImageArray()
     ImageInfo()
 
     init_gui()
