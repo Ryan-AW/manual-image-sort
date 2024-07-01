e87bbd2c784bf9ddb6c895bd096501c04f005ab0
Ryan-AW <ryanawhittock@outlook.com>
2024-07-01 06:23:04 +0100

    removed pop

--- utils/copy_move.py
+++ utils/copy_move.py
@@ -1,7 +1,7 @@
 def copy_move(input_file, *output_directories):
     if output_directories:
-        last_directory = output_directories.pop()
-        for directory in output_directories:
+        last_directory = output_directories[0]
+        for directory in output_directories[1:]:
             print('COPY:', input_file, '--->', directory)
         print('MOVE:', input_file, '--->', last_directory)
     else:
