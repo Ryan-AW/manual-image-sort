efecae581f9020ce7c7fe7da0db149d4e236f742
Ryan-AW <ryanawhittock@outlook.com>
2024-07-01 06:44:33 +0100

    implemented copy and move

--- utils/copy_move.py
+++ utils/copy_move.py
@@ -1,8 +1,11 @@
+from shutil import copy2, move
+
+
 def copy_move(input_file, *output_directories):
     if output_directories:
         last_directory = output_directories[0]
         for directory in output_directories[1:]:
-            print('COPY:', input_file, '--->', directory)
-        print('MOVE:', input_file, '--->', last_directory)
+            copy2(input_file, directory)
+        move(input_file, last_directory)
     else:
         raise KeyError('output directories must be specified')
