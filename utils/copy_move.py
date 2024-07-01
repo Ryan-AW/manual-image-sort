b346c710cc436904b7af401253f6c7a97656875d
Ryan-AW <ryanawhittock@outlook.com>
2024-07-01 06:47:28 +0100

    added docstrings

--- utils/copy_move.py
+++ utils/copy_move.py
@@ -1,7 +1,9 @@
+''' copy a file to multiple directories then delete the original file '''
 from shutil import copy2, move
 
 
 def copy_move(input_file, *output_directories):
+    ''' copy input_file to each of the output_directories then deletes the input_file '''
     if output_directories:
         last_directory = output_directories[0]
         for directory in output_directories[1:]:
